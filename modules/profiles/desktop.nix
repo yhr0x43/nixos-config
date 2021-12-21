9f85d84a8e80249dc50218d9bfb5d1aa51eaa6a0
yhr0x43 <yhr0x43@gmail.com>
2021-12-21 04:47:18 -0600

    Use enable options in modules

--- modules/profiles/desktop.nix
+++ modules/profiles/desktop.nix
@@ -2,95 +2,66 @@
 
 let
 
-  cfg = config.profile.x11;
-  unstable = import <nixos-unstable> {};
-  inherit (lib) optionals;
+  cfg = config.profile.desktop;
 
 in {
-  imports = [
-    ../services/gpg.nix
-  ];
-
-  sound.enable = true;
-  hardware.pulseaudio = {
-    enable = true;
-    package = pkgs.pulseaudioFull;
+  options.profile.desktop = {
+    enable = mkEnableOption "Turn on desktop profile";
   };
 
-  # Explicit PulseAudio support in applications
-  nixpkgs.config.pulseaudio = true;
-
-  hardware.bluetooth.enable = true;
-
-  services.xserver = {
-    enable = true;
-    windowManager.bspwm.enable = true;
-    #libinput.enable = true; #TODO What does libinput do exactly?
-  };
-
-  services.picom = {
-    enable = false;
-    #TODO picom config
-  };
-
-  services.dbus.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    polybar
-    firefox
-    flameshot
-    dunst
-    pavucontrol
-    dmenu
-    libnotify
-    zathura
-    alacritty
-    libreoffice
-  ];
-
-  fonts.fonts = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-  ];
-
-
-  systemd.user.services = {
-    polybar = {
-      description = "Polybar";
-      partOf = [ "graphical-session.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.polybar}/bin/polybar default"; 
-        ExecReload = "/usr/bin/kill -SIGUSR1 $MAINPID";
-      };
+  config = mkIf cfg.enable {
+    system.custom = {
+      fonts.enable = true;
+      i18n.enable = true;
+      syncthing.enable = true;
     };
 
-    flameshot = {
-      description = "Flameshot";
-      partOf = [ "graphical-session.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.flameshot}/bin/flameshot"; 
-      };
+    time.timeZone = "America/Chicago";
+
+    environment.systemPackages = with pkgs; [
+      python3 #TODO: for passFF
+      pavucontrol
+
+      # WM relavent derivation
+      dmenu
+      flameshot
+      j4-dmenu-desktop
+
+      xfce.thunar
+      lxmenu-data
+      shared_mime_info
+
+      # Archive manager
+      mate.engrampa
+    ];
+
+    #TODO: use u2f
+    security.pam.yubico = {
+      enable = true;
+      mode = "challenge-response";
     };
-  };
 
-  services.blueman.enable = true;
+    # Auto-mount USB drive
+    services.gvfs.enable = true;
 
-  services.syncthing = {
-    enable = true;
-    user = "yhrc";
-    dataDir = "/home/yhrc";
-    configDir = "/home/yhrc/.config/syncthing";
-    declarative.folders = {
-      "/home/yhrc/dox"                        .id = "rfkc3-ae2wq";
-      "/home/yhrc/.local/share/password-store".id = "nmdgd-nr5ik";
+    # Running GNOME program outside of GNOME
+    # Provide Dbus ca.desrt.dconf
+    programs.dconf.enable = true;
+
+    environment.variables = {
+      BROWSER = "firefox";
+      EDITOR = "nvim";
+
+      #TODO: more proper home-cleanup
+      IPYTHONDIR = "$XDG_CONFIG_HOME/jupyter";
+      JUPYTER_CONFIG_DIR = "$XDG_CONFIG_HOME/jupyter";
     };
-  };
 
-  services.pcscd.enable = true;
-
-  security.pam.yubico = {
-    enable = true;
-    mode = "challenge-response";
+    system.custom.mainUser = {
+      enable = true;
+      userName = "yhrc";
+      #TODO: manage wireshark in system.custom.mainUser
+      extraGroups = [ "wireshark" "video" "lp" "scanner" "dialout" ];
+    };
   };
 }
