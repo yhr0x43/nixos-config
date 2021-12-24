e6c1dec8ab593b36eebc1d6e88e48149e783512b
yhr0x43 <yhr0x43@gmail.com>
2021-12-24 00:09:21 -0600

    move udevs to a common file

--- hardware/cybermega.nix
+++ hardware/cybermega.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, ... }:
 
 {
-  imports = [ ./common/zfs.nix ];
+  imports = [ ./common/zfs.nix ./common/udev.nix ];
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/0A5E-C2D1";
@@ -41,22 +41,6 @@
   environment.systemPackages = with pkgs; [
     hackrf
   ];
-
-  services.udev.extraRules = ''
-    # HackRF Jawbreaker
-    ATTR{idVendor}=="1d50", ATTR{idProduct}=="604b", SYMLINK+="hackrf-jawbreaker-%k", MODE="660", TAG+="uaccess"
-    # HackRF One
-    ATTR{idVendor}=="1d50", ATTR{idProduct}=="6089", SYMLINK+="hackrf-one-%k", MODE="660", TAG+="uaccess"
-    # rad1o
-    ATTR{idVendor}=="1d50", ATTR{idProduct}=="cc15", SYMLINK+="rad1o-%k", MODE="660", TAG+="uaccess"
-    # NXP Semiconductors DFU mode (HackRF and rad1o)
-    ATTR{idVendor}=="1fc9", ATTR{idProduct}=="000c", SYMLINK+="nxp-dfu-%k", MODE="660", TAG+="uaccess"
-    # rad1o "full flash" mode
-    KERNEL=="sd?", SUBSYSTEM=="block", ENV{ID_VENDOR_ID}=="1fc9", ENV{ID_MODEL_ID}=="0042", SYMLINK+="rad1o-flash-%k", MODE="660", TAG+="uaccess"
-    # rad1o flash disk
-    KERNEL=="sd?", SUBSYSTEM=="block", ENV{ID_VENDOR_ID}=="1fc9", ENV{ID_MODEL_ID}=="0082", SYMLINK+="rad1o-msc-%k", MODE="660", TAG+="uaccess"
-    #
-  '';
 
   nixpkgs.config.pulseaudio = true;
 }
