52a95dcd61afbddfb31370d58b20636fe47d66e4
yhr0x43 <yhr0x43@gmail.com>
2022-01-10 03:24:21 -0600

    update tpx1c with modules

--- boxes/tpx1c.nix
+++ boxes/tpx1c.nix
@@ -1,10 +1,20 @@
-{ lib, ... }: {
+{ lib, ... }:
 
-  imports = [ ../roles/workstation.nix ../roles/gaming.nix ../hardware/tpx1c.nix ];
+{
+
+  profile.workstation.enable = true;
+
+  system.custom.fs.bootUuid = "0D56-B89B";
+
+  systemd.tmpfiles.rules = [
+    "L /etc/NetworkManager/system-connections - - - - /persist/etc/NetworkManager/system-connections"
+  ];
 
   boot = {
     supportedFilesystems = [ "zfs" ];
     zfs.requestEncryptionCredentials = true;
+    initrd.availableKernelModules =
+      [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
     loader = {
       efi.canTouchEfiVariables = true;
       grub = {
@@ -12,12 +22,10 @@
         efiSupport = true;
         version = 2;
         devices = [ "nodev" ];
-
-        # https://nixos.wiki/wiki/NixOS_on_ZFS
-        copyKernels = true;
       };
     };
   };
+
   networking.networkmanager.enable = true;
   networking.interfaces.wlp0s20f3.useDHCP = true;
 
