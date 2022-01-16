cbeb253ccffdbc7bd05dc43fea630aa88010cccd
yhr0x43 <yhr0x43@gmail.com>
2022-01-16 07:48:41 -0600

    switch to pipewire

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -9,30 +9,41 @@ let
 in {
 
   options.system.custom.audio = {
-    enable = mkEnableOption "use PluseAudio";
+    enable = mkEnableOption "use audio";
   };
 
   config = mkIf cfg.enable {
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+
+
     # because of systemWide ensure main user is in audio group
     #system.custom.mainUser.extraGroups = [ "audio" ];
 
-    hardware.pulseaudio = {
-      enable = true;
-      package = pkgs.pulseaudioFull;
+    #hardware.pulseaudio = {
+    #  enable = true;
+    #  package = pkgs.pulseaudioFull;
 
-      # all in audio group can do audio
-      # systemWide = true;
+    #  # all in audio group can do audio
+    #  # systemWide = true;
 
-      extraConfig = ''
-        # automatically switch to newly-connected devices
-        load-module module-switch-on-connect
-      '';
-    };
+    #  extraConfig = ''
+    #    # automatically switch to newly-connected devices
+    #    load-module module-switch-on-connect
+    #  '';
+    #};
 
-    nixpkgs.config.pulseaudio = true;
+    #nixpkgs.config.pulseaudio = true;
 
     # Packages needed
     # ---------------
+    #TODO: maybe tryout more interesting tools?
     environment.systemPackages = with pkgs; [
       pavucontrol
     ];
