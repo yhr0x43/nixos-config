b5603be09c37918e304770e4583303d2734f417d
yhr0x43 <yhr0x43@gmail.com>
2023-11-07 15:19:42 -0600

    remove sway setup, seems to interfere with river

--- boxes/frame.nix
+++ boxes/frame.nix
@@ -4,8 +4,6 @@
 
   profile.workstation.enable = true;
 
-  programs.custom.sway.enable = true;
-
   services.fwupd.enable = true;
 
   # FIXME: /etc/tmpfiles.d/00-nixos.conf:17: Duplicate line for path "/etc/NetworkManager/system-connections", ignoring.
